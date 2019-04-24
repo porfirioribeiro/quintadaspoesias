8c48072d820e250592e2a5996240eadbdce13f9e
Porfirio Ribeiro <porfirioribeiro@gmail.com>
2019-04-24 18:40:24 +0100

    site-icon and manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'Quinta das Poesias',
+        short_name: 'Q. das Poesias',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
